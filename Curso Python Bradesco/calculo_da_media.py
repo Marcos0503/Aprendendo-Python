68a0c6d7e8708b0d404119c6749325868b4bb387
Marcos0503 <Marcoshenrique96126638@gmail.com>
2024-09-22 00:57:09 -0300

    Adicionando uam nova aula

--- Curso Python Bradesco/calculo_da_media.py
+++ Curso Python Bradesco/calculo_da_media.py
@@ -2,11 +2,12 @@ def lernotas():
     n = float(input('Digite uma nota para o aluno(a): '))
     return n
 
-def media(n1,n2):
-    resultado = (n1+n2)/2
+def media(n1,n2,n3):
+    resultado = (n1+n2+n3)/3
     print("Nota 1: ", n1)
     print("Nota 2: ", n2)
-    print("Media: ", resultado)
+    print("Nota 3: ", n3)
+    print("Media: %.1f"% resultado)
     if resultado >= 7:
         print("Aprovado")
     else:
@@ -14,4 +15,5 @@ def media(n1,n2):
 
 a = lernotas()
 b = lernotas()
-media(a,b)
+c = lernotas()
+media(a,b,c)
